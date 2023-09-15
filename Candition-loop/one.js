77b1990d8b5293f9cac8e5c818c79fc521a57e56
Arzoo Raza <142464887+ArzoRaza@users.noreply.github.com>
2023-09-15 08:10:21 +0530

    condition or loop have in one file

---
+++ Candition-loop/one.js
@@ -0,0 +1,72 @@
+// let age = 17;
+// if(age >= 18){
+//     console.log("You are Allowed");
+// }
+// else{
+//     console.log("You are Not Allowed")
+// }
+
+// let's we see create one example 
+
+// let signal = "blue"; 
+// if(signal == "red") {
+//     console.log("stop");
+// }
+// else if(signal == "yellow") {
+//     console.log("Go slow");
+// }
+// else if(signal == "green") {
+//     console.log("Go Fast");
+// }
+// else {
+//     console.log("Invalid");
+// }
+
+
+// switch case 
+
+// let user = "man"
+// switch (user) {
+//     case "admin":
+//         console.log("I'm a admin");
+//         break;
+
+//     case "student":
+//         console.log("I am a student");
+//         break;
+
+//     case "mantor":
+//         console.log("I am a mantor");
+//         break;
+
+//     default:
+//         console.log("I am a default");   
+// }
+
+// loop 
+
+// for loop 
+/* for(let i = 0; i < 5; i++){
+    console.log(i);>
+} */
+ 
+// do while loop
+let i = 0;
+do{
+    // console.log("Hello world");
+    i++;
+}while(i <= 7)
+
+
+// while loop
+let k = 0;
+while(k > 10) {
+    // console.log("Hello world");
+    k++;
+}
+
+
+
+// Ternary operators - Ternary canditions
+let isloggedin = true;
+isloggedin ? console.log("Home page") : console.log("login Page");
